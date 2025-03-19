b34cf1c717906d8231b97df681144940a83c359b
vivekprasanna <vivekprasanna.prabhu@gmail.com>
2025-03-19 10:43:14 -0400

    pulling images from drive instead of local path

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -128,5 +128,8 @@ def get_device() -> torch.device:
         device = torch.device("mps")
     elif torch.cuda.is_available():
         device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
     print("Device: ", device)
     return device
